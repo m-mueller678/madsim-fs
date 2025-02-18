491ce092c2a8d52a1cba76f1138ccba23bcbbab6
m-mueller678 <marcus.mueller.678@gmail.com>
2025-02-18 12:26:50 +0100

    wip

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,17 @@ use atomic_refcell::AtomicRefCell;
 use madsim::plugin::{node, simulator, Simulator};
 use madsim::rand::GlobalRng;
 use madsim::task::{NodeId, Spawner};
-use madsim::time::TimeHandle;
+use madsim::time::{Sleep, TimeHandle};
 use madsim::Config;
 use snap_buf::SnapBuf;
 use std::collections::{HashMap, VecDeque};
+use std::future::Future;
 use std::io::{Error, Seek, SeekFrom, Write};
 use std::path::{Path, PathBuf};
 use std::pin::Pin;
 use std::sync::{Arc, Mutex};
-use std::task::{Context, Poll};
-use std::time::Instant;
+use std::task::{ready, Context, Poll};
+use std::time::{Duration, Instant};
 use tokio::io::{AsyncSeek, AsyncWrite};
 
 mod file_contents;
@@ -19,33 +20,81 @@ mod file_contents;
 #[cfg(madsim)]
 struct FsSimulator {
     file_systems: Mutex<HashMap<NodeId, Filesystem>>,
+    config: Arc<AtomicRefCell<FsConfig>>,
+}
+
+struct FsConfig {
+    write_delay: Duration,
+    flush_delay: Duration,
+    allow_dirty_write: bool,
+}
+
+impl FsConfig {
+    fn flush_sleep(&self, write_time: Instant) -> Option<Sleep> {
+        let flush_time = TimeHandle::current().now_instant();
+        let until = (flush_time + self.flush_delay).max(write_time + self.write_delay);
+        if until == flush_time {
+            None
+        } else {
+            Some(madsim::time::sleep_until(until))
+        }
+    }
 }
 
 #[derive(Clone)]
 pub struct FileHistory {
-    history: Arc<AtomicRefCell<VecDeque<FileHistoryEntry>>>,
+    history: Arc<AtomicRefCell<FileState>>,
 }
 
-impl FileHistory {
-    fn update<R>(&self, f: impl FnOnce(&FileSnapshot, u64) -> (Option<FileSnapshot>, R)) -> R {
-        let mut history = self.history.borrow_mut();
-        let back = history.back().unwrap();
-        let new_version = back.version + 1;
-        let (new_file, ret) = f(&back.file, new_version);
-        history.push_back(FileHistoryEntry {
-            version: new_version,
-            written: TimeHandle::current().now_instant(),
-            file: new_file,
-        });
-        ret
+enum FileState {
+    Clean(SnapBuf),
+    Written {
+        old: SnapBuf,
+        new: SnapBuf,
+        time: Instant,
+    },
+    Flush {
+        old: SnapBuf,
+        new: SnapBuf,
+        sleep: madsim::time::Sleep,
+    },
+}
+
+impl FileState {
+    fn new(&self) -> &SnapBuf {
+        match self {
+            FileState::Clean(new)
+            | FileState::Written { new, .. }
+            | FileState::Flush { new, .. } => new,
+        }
     }
 
-    fn history_len(&self) -> usize {
-        self.history.borrow().len()
-    }
-
-    fn inspect<R>(&self, f: impl FnOnce(&FileSnapshot) -> R) -> R {
-        f(&self.history.borrow().back().unwrap().file)
+    fn poll_flush(
+        &mut self,
+        cx: &mut Context<'_>,
+        config: &AtomicRefCell<FsConfig>,
+    ) -> Poll<Result<(), Error>> {
+        loop {
+            match self {
+                FileState::Clean(x) => return Poll::Ready(Ok(())),
+                FileState::Written { old, new, time } => {
+                    if let Some(sleep) = config.borrow().flush_sleep(*time) {
+                        *self = FileState::Flush {
+                            old: old.clone(),
+                            new: new.clone(),
+                            sleep,
+                        }
+                    } else {
+                        *self = FileState::Clean(new.clone())
+                    }
+                }
+                FileState::Flush { old, new, sleep } => {
+                    ready!(Pin::new(sleep).poll(cx));
+                    *self = FileState::Clean(new.clone());
+                    return Poll::Ready(Ok(()));
+                }
+            }
+        }
     }
 }
 
@@ -97,6 +146,11 @@ impl Simulator for FsSimulator {
     {
         Self {
             file_systems: Mutex::new(Default::default()),
+            config: Arc::new(AtomicRefCell::new(FsConfig {
+                write_delay: Duration::ZERO,
+                flush_delay: Duration::ZERO,
+                allow_dirty_write: true,
+            })),
         }
     }
 
@@ -125,10 +179,11 @@ pub struct FileHandle {
     pendig_seek: Option<i64>,
     cursor: usize,
     history: FileHistory,
+    config: Arc<AtomicRefCell<FsConfig>>,
 }
 
 impl AsyncSeek for FileHandle {
-    fn start_seek(self: Pin<&mut Self>, pos: SeekFrom) -> std::io::Result<()> {
+    fn start_seek(mut self: Pin<&mut Self>, pos: SeekFrom) -> std::io::Result<()> {
         if self.pendig_seek.is_some() {
             return Err(std::io::Error::new(
                 std::io::ErrorKind::Other,
@@ -137,13 +192,17 @@ impl AsyncSeek for FileHandle {
         }
         let new = match pos {
             SeekFrom::Start(x) => x as i64,
-            SeekFrom::End(x) => self.history.inspect(|f| f.content.len()) as i64 + x,
+            SeekFrom::End(x) => self.history.history.borrow().new().len() as i64 + x,
             SeekFrom::Current(x) => self.cursor as i64 + x,
         };
         self.pendig_seek = Some(new);
+        Ok(())
     }
 
-    fn poll_complete(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<std::io::Result<u64>> {
+    fn poll_complete(
+        mut self: Pin<&mut Self>,
+        _cx: &mut Context<'_>,
+    ) -> Poll<std::io::Result<u64>> {
         let new = self.pendig_seek.take().unwrap();
         Poll::Ready(if new < 0 {
             Err(std::io::Error::new(
@@ -158,30 +217,56 @@ impl AsyncSeek for FileHandle {
 }
 
 impl AsyncWrite for FileHandle {
-    fn flush(&mut self) -> std::io::Result<()> {}
-
     fn poll_write(
-        self: Pin<&mut Self>,
+        mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<Result<usize, Error>> {
-        if self.history.history_len() > 3 {
-            return Poll::Pending;
+        let now = TimeHandle::current().now_instant();
+        loop {
+            let mut this = self.history.history.borrow_mut();
+            match &mut *this {
+                FileState::Clean(x) => {
+                    let mut new = x.clone();
+                    new.write(self.cursor, buf);
+                    *this = FileState::Written {
+                        old: x.clone(),
+                        new,
+                        time: now,
+                    };
+                    break;
+                }
+                FileState::Written { old, new, time } => {
+                    if self.config.borrow().allow_dirty_write {
+                        new.write(self.cursor, buf);
+                        *time = now;
+                        break;
+                    } else {
+                        drop(this);
+                        ready!(self.as_mut().poll_flush(cx));
+                    }
+                }
+                FileState::Flush { old, new, sleep } => {
+                    *this = FileState::Written {
+                        old: old.clone(),
+                        new: new.clone(),
+                        time: now,
+                    }
+                }
+            }
         }
-        self.history.update(|f, _| {
-            let mut content = f.content.clone();
-            content.write(self.cursor, buf);
-            let f = FileSnapshot { content };
-            (f, ())
-        });
-        Ok(buf.len())
+        self.cursor += buf.len();
+        Poll::Ready(Ok(buf.len()))
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        todo!()
+        self.history
+            .history
+            .borrow_mut()
+            .poll_flush(cx, &*self.config)
     }
 
     fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Error>> {
-        todo!()
+        self.poll_flush(cx)
     }
 }
